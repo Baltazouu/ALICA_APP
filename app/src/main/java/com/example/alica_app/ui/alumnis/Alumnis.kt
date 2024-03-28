225debde9abecac8d5aa9ff0b2a53cf6f51038d9
Alexis FERON <Alexis.FERON@etu.uca.fr>
2024-03-28 09:52:08 +0100

    begin of searchbar

--- app/src/main/java/com/example/alica_app/ui/alumnis/Alumnis.kt
+++ app/src/main/java/com/example/alica_app/ui/alumnis/Alumnis.kt
@@ -1,25 +1,30 @@
 package com.example.alica_app.ui.alumnis
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 
@@ -28,6 +33,9 @@ fun Alumnis(navController: NavController) {
     LazyColumn(
         modifier = Modifier.fillMaxWidth()
     ) {
+        item{
+            SearchBar()
+        }
         items(10) {
             AlumniCard()
             Spacer(modifier = Modifier.height(10.dp))
@@ -43,10 +51,12 @@ fun AlumniCard(){
         .fillMaxWidth()
         .height(50.dp), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Absolute.Left) {
         Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.Start,
-            modifier = Modifier.padding(5.dp).fillMaxWidth()) {
+            modifier = Modifier
+                .padding(5.dp)
+                .fillMaxWidth()) {
             Row (verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center ){
                 Column {
-                    Icon(Icons.Filled.AccountCircle, contentDescription = "Profil icon")
+                    Icon(Icons.Outlined.AccountCircle, contentDescription = "Profil icon")
                 }
                 Spacer(modifier = Modifier.padding(10.dp))
                 Column {
@@ -61,6 +71,20 @@ fun AlumniCard(){
             }
         }
     }
+}
 
+@Composable
+fun SearchBar() {
+    var text by remember { mutableStateOf("") }
+    Row {
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Search") },
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Icon(Icons.Outlined.Search, contentDescription = "Search icon")
+        }
+    }
 
 }
