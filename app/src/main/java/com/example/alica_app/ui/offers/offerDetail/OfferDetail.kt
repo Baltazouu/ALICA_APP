d06df153a4836c5027462527d26e219407a26c0c
tleodev <leo.tuaillon@etu.uca.fr>
2024-04-01 11:02:25 +0200

    detail offer view

--- app/src/main/java/com/example/alica_app/ui/offers/offerDetail/OfferDetail.kt
+++ app/src/main/java/com/example/alica_app/ui/offers/offerDetail/OfferDetail.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
@@ -59,26 +60,45 @@ fun OfferDetail() {
             }
         }
         item {Modifier.padding(20.dp)
-            Box(modifier = Modifier.padding(10.dp)){
-                Text(text = "Descriptif du poste :")
-                Text(text = "CECI EST LA DESCRIPtion de test \n test\n\t test",
-                    modifier = Modifier
-                        .background(color = Color.Cyan)
-                        .padding(5.dp)
-                        .fillMaxWidth(1f)
-                )
+            Column {
+                boxSpec(subtitle = "Desciptif du poste :",
+                    content = "Nous recherchons un développeur Android pour un stage de 6 mois. Vous serez en charge de la création d'une application mobile pour notre entreprise.")
+                boxSpec(subtitle = "Profil recherché :",
+                    content = "Vous êtes étudiant en informatique et vous avez des compétences en développement Android. Vous êtes autonome et vous avez un bon relationnel.")
+                boxSpec(subtitle = "Postuler :",
+                    content = "cgi@recrutement.com | 04 73 00 00 00 | https://www.cgi.fr/")
             }
+
+        }
+    }
+}
+
+
+@Composable
+fun boxSpec(subtitle:String = "TEXT", content:String = "TEXT"){
+    Box(modifier = Modifier.padding(10.dp)) {
+        Column {
+            Text(text = subtitle, fontSize = 20.sp, modifier = Modifier.padding(5.dp))
+            Text(
+                text = content,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(color = Color.Cyan)
+                    .padding(5.dp)
+                    .fillMaxWidth(1f)
+            )
         }
     }
 }
 
 
 
-
 @Composable
 fun offerSpec(text:String = "TEXT"){
         Text(text = text, modifier = Modifier
+            .clip(RoundedCornerShape(10.dp))
             .background(Color.Gray)
-            .padding(5.dp))
+            .padding(5.dp)
+        )
 
 }
