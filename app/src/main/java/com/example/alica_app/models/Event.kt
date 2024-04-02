3789d924b5e2c3abdc35a4ba24ad22a0f318661a
ralacote <Raphael.LACOTE@etu.uca.fr>
2024-04-02 16:36:33 +0200

    Ajout de la classe et amélioration des events

---
+++ app/src/main/java/com/example/alica_app/models/Event.kt
@@ -0,0 +1,5 @@
+package com.example.alica_app.models
+
+import java.util.Date
+
+class Event(var Name : String, var Site : String, var Date : String, var Subscriber : Int, var Limit : Int)
--- app/src/main/java/com/example/alica_app/ui/evenements/Evenements.kt
+++ app/src/main/java/com/example/alica_app/ui/evenements/Evenements.kt
@@ -26,28 +26,36 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.alica_app.R
+import com.example.alica_app.models.Event
+import java.util.Date
 
 @Composable
 fun Evenements() {
+
+    val allEvent : List<Event> = listOf(
+        Event("Laser Game","Clermont-Ferrand", "24/09/2015",25,12),
+        Event("Laser Game","Paris", "24/09/2015",25,12),
+        Event("Patinage","Clermont", "24/10/2015",25,12),
+        Event("Laser Game","Orléans", "24/09/2015",32,12)
+    )
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(Dp(10f)), horizontalAlignment = Alignment.CenterHorizontally
+            .padding(Dp(15f)), horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text(
             text = stringResource(R.string.event),
             fontSize = 35.sp,
             textAlign = TextAlign.Center
         )
-        OneEvent()
-        OneEvent()
-        OneEvent()
+        for (e in allEvent){
+            OneEvent(e)
+        }
     }
 }
 
-@Preview
 @Composable
-fun OneEvent(){
+fun OneEvent(e : Event){
     Column {
         Row {
             AsyncImage(
@@ -59,11 +67,11 @@ fun OneEvent(){
                 )
             Spacer(modifier = Modifier.width(20.dp))
             Column {
-                Field(R.string.test_name)
-                Field(R.string.test_city)
-                Field(R.string.test_date)
-                Field(R.string.test_limite)
-                Field(R.string.test_inscrit)
+                Field(e.Name)
+                Field(e.Site)
+                Field(e.Date)
+                Field(e.Subscriber.toString())
+                Field(e.Limit.toString())
             }
         }
         Row{
@@ -79,10 +87,10 @@ fun OneEvent(){
 }
 
 @Composable
-fun Field(resources : Int){
+fun Field(resources : String){
     Text(
-        text = stringResource(resources),
-        fontSize = 20.sp,
+        text = resources,
+        fontSize = 18.sp,
         textAlign = TextAlign.Center
     )
 }
